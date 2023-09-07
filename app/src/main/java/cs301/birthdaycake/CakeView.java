493fa87871c18aab2abf6d6be935d6c977bc12b0
sprigg26 <sprigg26@up.edu>
2023-09-07 13:52:58 -0700

    Added new candle

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -71,10 +71,13 @@ public class CakeView extends SurfaceView {
     public void drawCandle(Canvas canvas, float left, float bottom) {
         canvas.drawRect(left, bottom - candleHeight, left + candleWidth, bottom, candlePaint);
 
+
         //draw the outer flame
         float flameCenterX = left + candleWidth/2;
         float flameCenterY = bottom - wickHeight - candleHeight - outerFlameRadius/3;
         canvas.drawCircle(flameCenterX, flameCenterY, outerFlameRadius, outerFlamePaint);
+
+
 
         //draw the inner flame
         flameCenterY += outerFlameRadius/3;
@@ -84,6 +87,7 @@ public class CakeView extends SurfaceView {
         float wickLeft = left + candleWidth/2 - wickWidth/2;
         float wickTop = bottom - wickHeight - candleHeight;
         canvas.drawRect(wickLeft, wickTop, wickLeft + wickWidth, wickTop + wickHeight, wickPaint);
+
 
     }
 
@@ -120,7 +124,8 @@ public class CakeView extends SurfaceView {
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
         //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
+        drawCandle(canvas, cakeLeft+100 + cakeWidth/2 - candleWidth/2, cakeTop);
+        drawCandle(canvas, cakeLeft-300 + cakeWidth/2 - candleWidth/2, cakeTop);
 
     }//onDraw
 
