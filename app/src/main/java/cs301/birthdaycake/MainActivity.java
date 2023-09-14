b90845759bcb64b8f7dbbcba8a917abe7ce9a555
sprigg26 <sprigg26@up.edu>
2023-09-14 12:54:21 -0700

    birthday cake checkpoint 5

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -3,6 +3,8 @@ package cs301.birthdaycake;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goodbye(View button) {
+        Log.i("button", "Goodbye");
+    }
+
 }
